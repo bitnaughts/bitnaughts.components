b9e91ec20418bd4fd52aa80ea610bb29490b4a7f
Brian Hungerman <brianhungerman@gmail.com>
2022-12-29 22:03:04 -0800

    lasers

--- SensorController.cs
+++ SensorController.cs
@@ -10,40 +10,54 @@ public class SensorController : ComponentController {   //RangeFinder == 1D, Sca
     // public float max_step = 25f;
     public bool organicSensor;
     private float distance = 0, distance_min = 0, distance_max = 999, scan_range = 0;    
+    public GameObject Laser;
+    private int laser_count = 0;
     public override void Focus() {}
     public override void Ping() {
     }
     public override float Action (float input) 
     {
         if (this == null) { Destroy(this); return -999; }
-        // var random = new System.Random();
-        // return (float)(random.NextDouble() * 1998f - 999f); 
 
-        // Bit shift the index of the layer (8) to get a bit mask
-        int asteroidMask = 1 << 3, enemyMask = 1 << 6; 
+        // laser addition
 
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        asteroidMask = ~asteroidMask;
-        enemyMask = ~enemyMask;
+        if (input == -1 ){ 
 
-        RaycastHit hit;
-        float asteroidDistance = 0f, enemyDistance = 0f;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, asteroidMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit asteroidMask");
-            asteroidDistance = hit.distance;
-        }
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemyMask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit enemyMask");
-            enemyDistance = hit.distance;
-        }
-        if (asteroidDistance < enemyDistance) return asteroidDistance;
-        else return -enemyDistance; // negative denotes "enemy" to sensor
+            Fire();
+
+
+            return -1;
+        } else {
+            
+            // var random = new System.Random();
+            // return (float)(random.NextDouble() * 1998f - 999f); 
+
+            // Bit shift the index of the layer (8) to get a bit mask
+            int asteroidMask = 1 << 3, enemyMask = 1 << 6; 
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            asteroidMask = ~asteroidMask;
+            enemyMask = ~enemyMask;
+
+            RaycastHit hit;
+            float asteroidDistance = 0f, enemyDistance = 0f;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, asteroidMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit asteroidMask");
+                asteroidDistance = hit.distance;
+            }
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemyMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit enemyMask");
+                enemyDistance = hit.distance;
+            }
+            if (asteroidDistance < enemyDistance) return asteroidDistance;
+            else return -enemyDistance;
+        }// negative denotes "enemy" to sensor
         // LayerMask mask = LayerMask.GetMask("Default"); //Larger sensors can detect more layers of detail (see individual components, shells, etc.)
         // RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up * 2, Vector2.up * 999,999, mask, -999, 999);
         // if (hit != null && hit.collider != null)
@@ -57,6 +71,32 @@ public class SensorController : ComponentController {   //RangeFinder == 1D, Sca
         //     distance = distance_max;
         //     // Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward), Color.green, .001f, false);
         //     return distance_max;
+    }
+    public void Fire () {
+        // for (int i = 0; i < reload_timer.Length; i++) {
+            // if (reload_timer[i] <= 0)
+            // {
+                // Interactor.Sound("Laser");// + ((i % 2) + 1));
+                // reload_timer[i] = Mathf.FloorToInt(GetComponent<SpriteRenderer>().size.y - 1);
+                GameObject laser = Instantiate(
+                    Laser,
+                    this.transform.position,
+                    this.transform.rotation,      
+                    this.transform
+                ) as GameObject;
+                laser.transform.Translate(new Vector3(0, (-0.5f * GetComponent<SpriteRenderer>().size.y) + 2)); //new Vector3((i + .5f) - reload_timer.Length / 2f
+                laser.name = "l" + this.name + laser_count++;
+                laser.transform.SetParent(GameObject.Find("World").transform);
+                // laser.GetComponent<ProjectileController>().speed = 1000;//GetComponent<SpriteRenderer>().size.y + (GetComponentInParent<StructureController>().translation.magnitude * 7f);
+                // laser.GetComponent<ProjectileController>().acceleration = 1f / GetComponent<SpriteRenderer>().size.y;
+                // laser.GetComponent<ProjectileController>().damage = Mathf.FloorToInt(GetComponent<SpriteRenderer>().size.y - 1) * 2;
+                // laser.GetComponent<SpriteRenderer>().size = new Vector2 (.65f, GetComponent<SpriteRenderer>().size.y);
+                // var sh = laser.GetComponent<ParticleSystem>().shape;
+                // sh.position = new Vector2(0, -(GetComponent<SpriteRenderer>().size.y / 2));
+                return;
+            // }
+        // }
+        
     }
     public override Vector2 GetMinimumSize ()
     {
